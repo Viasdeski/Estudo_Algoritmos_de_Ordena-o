a699606f645da672e0730d91cadcd5a3585765b6
Viasdeski <86928498+Viasdeski@users.noreply.github.com>
2022-06-04 18:24:02 -0300

    Algoritmos de Ordenacao

---
+++ Algoritmos_de_Ordenacao/mergeSort.py
@@ -0,0 +1,37 @@
+def mergeSort(lista):
+
+    if len(lista) > 1:
+        mid = len(lista)//2
+        lefthalf = lista[:mid]
+        righthalf = lista[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                lista[k]=lefthalf[i]
+                i=i+1
+            else:
+                lista[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            lista[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            lista[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",lista)
+
+lista = input("Insira os números separados por vírgula, os quais deseja ordenar: ")
+lista = list(lista)
+mergeSort(lista)
+print(lista)
